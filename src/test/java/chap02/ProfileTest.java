52f8d5986e92d54ac6ad3d9595b5c5a09dacc887
Timo <developer.timo@gmail.com>
2022-03-14 00:12:15 +0900

    [2장] JUnit 진짜로 써 보기
    - 구직자와 구인자의 기준(질문)에 대한 매칭 여부를 판단하는 matches 메서드의 첫 번째 단위테스트 생성

---
+++ src/test/java/chap02/ProfileTest.java
@@ -0,0 +1,26 @@
+package chap02;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class ProfileTest {
+
+    @Test
+    public void mathAnswerFalseWhenMustMatchCriteriaNotMet() {
+
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        Question question = new BooleanQuestion(1, "Got bonuses?");
+        Answer profileAnswer = new Answer(question, Boolean.FALSE);
+        profile.add(profileAnswer);
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Boolean.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+
+        assertFalse(matches);
+    }
+
+}
