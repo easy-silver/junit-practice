bb76fbb2611552dd27c925e57ef5570abc6fe9b4
Timo <developer.timo@gmail.com>
2022-03-14 00:25:10 +0900

    [2장] JUnit 진짜로 써 보기
    - 두 번째 단위 테스트 생성

--- src/test/java/chap02/ProfileTest.java
+++ src/test/java/chap02/ProfileTest.java
@@ -3,6 +3,7 @@ package chap02;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ProfileTest {
 
@@ -21,6 +22,24 @@ public class ProfileTest {
         boolean matches = profile.matches(criteria);
 
         assertFalse(matches);
+    }
+
+    @Test
+    public void matchAnswersTrueForAnyDontCareCriteria() {
+
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        Question question = new BooleanQuestion(1, "Got Milk?");
+        Answer profileAnswer = new Answer(question, Boolean.FALSE);
+        profile.add(profileAnswer);
+
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Boolean.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+
+        assertTrue(matches);
     }
 
 }
